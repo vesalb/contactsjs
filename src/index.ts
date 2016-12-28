963865cc09c754cec68a3ecc17bc3235d34f0f4b
Vesal Fayazi <vefa01@Betsson.local>
2016-12-28 13:13:12 +0100

    Changed so that the search gives a true output

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,13 @@ let agenda = new Agenda();
 
 let contact = new Contact('Vesal', 'Fayazi', 'Regeringsgatan 28');
 let contact1 = new Contact('Miguel', 'Dominguez', 'Spanish street 5');
-let contact2 = new Contact('Poop', 'Hat', 'Diarrhea alley 24');
+let contact2 = new Contact('Harry', 'Potter', 'Privet Drive 4');
 
 agenda.addContact(contact);
 agenda.addContact(contact1);
 agenda.addContact(contact2);
 
-let searchContact = agenda.searchInAgenda('Hatt');
+let searchContact = agenda.searchInAgenda('Potter');
 
 if (searchContact.length == 1){
     console.log(searchContact);
